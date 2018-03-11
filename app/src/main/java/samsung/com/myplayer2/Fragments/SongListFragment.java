6f2a9eb8b593c3e57f128543fce810570cab6181
tungvm96 <tungvu2796@gmail.com>
2018-03-11 10:32:29 +0700

    Cải thiện tốc độ tìm kiếm bài hát trong list

--- app/src/main/java/samsung/com/myplayer2/Fragments/SongListFragment.java
+++ app/src/main/java/samsung/com/myplayer2/Fragments/SongListFragment.java
@@ -9,7 +9,9 @@ import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.PorterDuff;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,11 +24,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
@@ -59,6 +64,7 @@ public class SongListFragment extends Fragment {
     }
 
     private ArrayList<Song> SongList;
+    private ArrayList<Song> SongFilterList;
     private RecyclerView songView;
     EditText searchbox;
     Context context;
@@ -75,6 +81,7 @@ public class SongListFragment extends Fragment {
     TextView textTimeSong;
     TextView textTotal;
     SeekBar seekBar;
+    ImageButton btnsearch;
 
     public void SetTimeTotal() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm:ss");
@@ -105,6 +112,8 @@ public class SongListFragment extends Fragment {
 
         btnPP = (ImageButton) getActivity().findViewById(R.id.btn_play_pause);
 
+        btnsearch = (ImageButton) v.findViewById(R.id.btnsearch);
+
         textTimeSong = (TextView) getActivity().findViewById(R.id.time_song);
 
         textTotal = (TextView) getActivity().findViewById(R.id.time_total);
@@ -118,6 +127,8 @@ public class SongListFragment extends Fragment {
         context = super.getActivity();
 
         SongList = new ArrayList<Song>();
+
+        SongFilterList = new ArrayList<Song>();
 
         getSongList();
 
@@ -135,21 +146,81 @@ public class SongListFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.length() == 0) {
-                    SongList.clear();
-                    getSongList();
-                    RecyclerSongAdapter songAdapter1 = new RecyclerSongAdapter(SongList);
-                    songView.setAdapter(songAdapter1);
-                } else {
+                    //SongList.clear();
+                    //getSongList();
+                    //RecyclerSongAdapter songAdapter1 = new RecyclerSongAdapter(SongList);
+                    //songView.setAdapter(songAdapter1);
+                } /*else {
                     songView.setAdapter(null);
                     SongList.clear();
                     getSongByName(charSequence.toString().toLowerCase());
                     RecyclerSongAdapter songAdapter2 = new RecyclerSongAdapter(SongList);
                     songView.setAdapter(songAdapter2);
-                }
+                }*/
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        searchbox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                  /* Write your logic here that will be executed when user taps next button */
+                    if (searchbox.getText() != null) {
+                        songView.setAdapter(null);
+                        SongFilterList.clear();
+                        getSongByName(searchbox.getText().toString().toLowerCase(), SongList);
+                        RecyclerSongAdapter songAdapter1 = new RecyclerSongAdapter(SongFilterList);
+                        songView.setAdapter(songAdapter1);
+                        myService.setList(SongFilterList);
+                    } else {
+                        songView.setAdapter(null);
+                        songView.setAdapter(songAdt);
+                        myService.setList(SongList);
+                    }
+                }
+                return false;
+            }
+        });
+
+        btnsearch.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageButton v = (ImageButton) view;
+                        v.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+
+                        // Your action here on button click
+                        if (searchbox.getText() != null) {
+                            songView.setAdapter(null);
+                            SongFilterList.clear();
+                            getSongByName(searchbox.getText().toString().toLowerCase(), SongList);
+                            RecyclerSongAdapter songAdapter2 = new RecyclerSongAdapter(SongFilterList);
+                            songView.setAdapter(songAdapter2);
+                            myService.setList(SongFilterList);
+                        } else {
+                            songView.setAdapter(null);
+                            songView.setAdapter(songAdt);
+                            myService.setList(SongList);
+                        }
+
+
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageButton v = (ImageButton) view;
+                        v.getBackground().clearColorFilter();
+                        v.invalidate();
+                        break;
+                    }
+                }
+                return false;
             }
         });
 
@@ -237,35 +308,11 @@ public class SongListFragment extends Fragment {
         musicCursor.close();
     }
 
-    public void getSongByName(String entry) {
-        //retrieve song info
-        ContentResolver musicResolver = getActivity().getContentResolver();
-        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
-        if (musicCursor != null && musicCursor.moveToFirst()) {
-            //get collumn
-            int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-            int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
-            int artisColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-            int dataColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
-            long coverid = musicCursor.getLong(musicCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
-            //add song to list
-            do {
-                long thisId = musicCursor.getLong(idColumn);
-                String thisTitle = musicCursor.getString(titleColumn);
-                String thisArtis = musicCursor.getString(artisColumn);
-                String thisData = musicCursor.getString(dataColumn);
-                Uri solidUri = Uri.parse("content://media/external/audio/");
-                Uri songUri = ContentUris.withAppendedId(solidUri, thisId);
-                Bitmap songimg = GetBitmap(thisData);
-                Bitmap lastimg = getResizedBitmap(songimg, 55, 60);
-                if (thisTitle.toLowerCase().contains(entry))
-                    SongList.add(new Song(thisId, thisTitle, thisArtis, null, lastimg, thisData));
-            }
-            while (musicCursor.moveToNext());
+    public void getSongByName(String entry, ArrayList<Song> entryArray) {
+        for (int i = 0; i < entryArray.size(); i++) {
+            if (entryArray.get(i).getTitle().toLowerCase().contains(entry) || entryArray.get(i).getArtist().toLowerCase().contains(entry))
+                SongFilterList.add(entryArray.get(i));
         }
-        SortByName();
-        musicCursor.close();
     }
 
     private ServiceConnection musicConnection = new ServiceConnection() {
